d307cc73571b9e97afcda4be2db6fa9270a0235c
Ben Meadors <benmmeadors@gmail.com>
2025-07-09 20:59:34 -0500

    Colors

--- components/LogoHeader.vue
+++ components/LogoHeader.vue
@@ -6,10 +6,11 @@
             <img src="@/public/img/icon_rak-cropped.svg" class="h-64 w-64 inline-block pt-0 mt-0" alt="RAK Logo" />
             {{ $t('header_title') }}
         </h1>
-        <h1 v-else class="text-white text-7xl inline-block ml-4 justify-center text-center text-meshtastic tracking-wide">
+        <h1 v-else class="text-white text-7xl inline-block ml-4 justify-center text-center tracking-wide">
             <img src="@/assets/img/logo.svg" class="h-32 w-32 inline-block pt-0 mt-0" alt="Meshtastic Logo" />
+            x
+            <img src="/img/OpenSauce_vector.svg" class="h-28 w-28 inline-block pt-0 mt-0 ml-4" alt="Open Sauce Logo" />
             {{ $t('header_title') }}
-            <img src="/img/OpenSauce_vector.svg" class="h-28 w-28 inline-block pt-0 mt-0 ml-4 opacity-80" alt="Open Sauce Logo" />
         </h1>
     </div>
 </template>
